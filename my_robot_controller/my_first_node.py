8f7f22be667d274b84093f276e3aed0f981cd738
dddddla <2082833060@qq.com>
2026-03-17 01:31:20 +0800

    study_for_topic_subscriber

--- my_robot_controller/my_first_node.py
+++ my_robot_controller/my_first_node.py
@@ -6,13 +6,17 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__('first_node')
-        self.get_logger().info('Hello, ROS2!')
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)  #创建一个定时器，每隔1秒调用一次timer_callback函数
 
+    def timer_callback(self):
+        self.get_logger().info('Timer callback triggered!'+str(self.counter_))  #在timer_callback函数中，使用get_logger()方法获取日志记录器，并调用info()方法输出一条信息
+        self.counter_+=1  #每次调用timer_callback函数时，counter变量会增加1
 
 def main(args=None):
     rclpy.init(args=args)
     node = MyNode()
-    rclpy.spin(node)
+    rclpy.spin(node)  #调用rclpy.spin()函数，使节点进入循环状态，等待事件发生并处理它们。当节点被关闭时，调用
     rclpy.shutdown()
 
 if __name__ == '__main__':
---
+++ my_robot_controller/pose_subscriber.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+
+class PoseSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('pose_subscriber')
+        self.subscription = self.create_subscription(
+            Pose,
+            '/turtle1/pose',
+            self.pose_callback,
+            10
+        )
+
+    def pose_callback(self, msg:Pose):
+        self.get_logger().info(str(msg))
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = PoseSubscriber()
+    rclpy.spin(node)
+    rclpy.shutdown()
